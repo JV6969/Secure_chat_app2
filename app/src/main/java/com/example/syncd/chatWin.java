1ccba8b4b36bf49f2d81b2956d99039e2a4d9bba
patelvatsal6605-ops <patelvatsal6605@gmail.com>
2025-10-25 10:51:15 +0530

    creating the chat section and the login bug fixes

--- app/src/main/java/com/example/syncd/chatWin.java
+++ app/src/main/java/com/example/syncd/chatWin.java
@@ -1,10 +1,12 @@
 package com.example.syncd;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,6 +19,10 @@ public class chatWin extends AppCompatActivity {
     String reciverimg, reciverUid,reciverName,SenderUID;
     CircleImageView profile;
     TextView reciverNName;
+    CardView sendbtn;
+    EditText textmsg;
+    public  static String senderImg;
+    public  static String reciverIImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +40,60 @@ public class chatWin extends AppCompatActivity {
         reciverimg = getIntent().getStringExtra("reciverImg");
         reciverUid = getIntent().getStringExtra("uid");
 
+        messagesArrayList = new ArrayList<>();
+
+        sendbtn = findViewById(R.id.sendbtnn);
+        textmsg = findViewById(R.id.textmsg);
+        reciverNName = findViewById(R.id.recivername);
+        profile = findViewById(R.id.profileimgg);
+        messageAdpter = findViewById(R.id.msgadpter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        messageAdpter.setLayoutManager(linearLayoutManager);
+        mmessagesAdpter = new messagesAdpter(chatwindo.this,messagesArrayList);
+        messageAdpter.setAdapter(mmessagesAdpter);
+
+
         profile = findViewById(R.id.profileimgg);
         reciverNName = findViewById(R.id.recivername);
 
         Picasso.get().load(reciverimg).into(profile);
         reciverNName.setText(""+reciverName);
+
+
+        sendbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = textmsg.getText().toString();
+                if (message.isEmpty()){
+                    Toast.makeText(chatwindo.this, "Enter The Message First", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                textmsg.setText("");
+                Date date = new Date();
+                msgModelclass messagess = new msgModelclass(message,SenderUID,date.getTime());
+
+                database=FirebaseDatabase.getInstance();
+                database.getReference().child("chats")
+                        .child(senderRoom)
+                        .child("messages")
+                        .push().setValue(messagess).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                database.getReference().child("chats")
+                                        .child(reciverRoom)
+                                        .child("messages")
+                                        .push().setValue(messagess).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+
 
 
 
